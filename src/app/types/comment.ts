b389270154ecb9f1195df23f238ed6e66a02fb63
Debugger3000 <carmac.ford@gmail.com>
2025-07-11 22:24:44 -0400

    comment nesting improved + delete

--- src/app/types/comment.ts
+++ src/app/types/comment.ts
@@ -36,6 +36,21 @@ export type LiveComment = {
   __v: number;
 }[];
 
+export type LiveCommentSolo = {
+  parentThread: string | null | undefined;
+  parentComment: string | null | undefined;
+  commentText: string | null | undefined;
+  owner: string | undefined;
+  childComments: LiveComment;
+  ownerUserName: string | null | undefined;
+  ownerPicture: string | null | undefined;
+  voteCount: number | null | undefined;
+  createdAt: string;
+  _id: string;
+  updatedAt: string;
+  __v: number;
+};
+
 export type commentVoteType = {
   commentId: string | null | undefined;
   voteType: boolean;
